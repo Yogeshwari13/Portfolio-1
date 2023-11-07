494307c67020399198f0f7865b3239d18baab319
Yogeshwari13 <yogeshwari.t@xerox.com>
2023-11-08 02:47:50 +0530

    Nav bar and responsieness

---
+++ src/components/HeroImg.js
@@ -0,0 +1,24 @@
+import "./HeroStyles.css";
+import React from 'react'
+import img from '../assets/2202758.jpg'
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="IntroImg" src={img} alt="IntroImg"/>
+        </div>
+        <div className="content">
+            <p>HI, I'M A FREELANCER</p>
+            <h1>React Developer.</h1>
+            <div>
+                <Link to="/projects" className="btn">Projects</Link>
+                <Link to="/contact" className="btn btn-light">Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroImg
