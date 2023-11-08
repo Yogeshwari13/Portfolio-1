1104796b002260e922388539ddc108ececf0178a
Yogeshwari13 <yogeshwari.t@xerox.com>
2023-11-09 01:34:02 +0530

    Completed version 1 of the portfolio app

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,9 +1,41 @@
+import { Link } from 'react-router-dom';
 import './AboutContentStyles.css';
 import React from 'react'
 
+import React1 from '../assets/react1.jpg'
+import React2 from '../assets/react2.webp'
+
 const AboutContent = () => {
   return (
-    <div>AboutContent</div>
+    <div className='about'>
+      <div className='left'>
+        <h1>
+          Who Am I?
+        </h1>
+        <p>
+        I'm a React front-end developer. I create responsive secure websites
+          for my clients.
+        </p>
+        <Link to="/contact">
+          <button className='btn'>
+              Contact
+          </button>
+      </Link>
+      </div>
+      <div className='right'>
+        <div className='img-container'>
+          <div className='img-stack top'>
+            <img src={React1} className='img' alt="react1"/>
+
+          </div>
+          <div className='img-stack bottom'>
+            <img src={React2} className='img' alt="react2"/>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
